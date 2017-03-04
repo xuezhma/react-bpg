db27f2bda9b7909880904376b443b70cc6ac6b97
xuezhma <xuezhma@gmail.com>
2017-03-03 19:35:52 -0800

    install bpg decoders

--- src/react-bpg.js
+++ src/react-bpg.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
+import  bpgDecoders from 'bpg-decoder'
 
 class ReactBPG extends Component {
     constructor(props) {
         super(props)
+        this.BPGDecoder = bpgDecoders[this.props.decoder || "BPGDecoder"]
     }
 
     componentDidMount() {
         const ctx = this.refs.canvas.getContext('2d')
-        const img = new BPGDecoder(ctx);
+        const img = new this.BPGDecoder(ctx);
         const self = this
         img.onload = function() {
             self.refs.canvas.width = this.imageData.width
