69d0ea77865aeae186c9723fbd26f33de7573f17
xuezhma <xuezhma@gmail.com>
2017-03-06 11:59:56 -0800

    Add an demo

---
+++ example/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import lena512color from './lena512color.bpg';
+import './App.css';
+import ReactBPG from 'react-bpg'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <ReactBPG src={lena512color} BPGDecoder="BPGDecoder" className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
